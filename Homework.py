ccf3f3908c1119e866d6f070c044e8033c96be06
vmirkash <marynavoronova@gmail.com>
2022-04-15 16:21:52 +0300

    Homework part2

---
+++ Homework.py
@@ -0,0 +1,39 @@
+# test_project
+import this
+
+# task 1
+str1 = 'text bla bla. and bla maybe bla'
+print (str1.isdigit())
+print(str1.count(' '))
+print(str1.count('.'))
+str2 = 'Homework'
+
+print(str2.title())
+
+print(str2.index('a'))
+print(str2.split(' '))
+print(str2.lstrip())
+print(str2.rstrip())
+
+# task 2
+
+
+
+# task 3
+l = [1, 2, 3, "abc"]
+print(l[-3])
+l2 = ["abc", "banana", 2, 4, "abc"]
+print(l2[1][1])
+print(l2[-1][-1])
+print(l2.append("dna"))
+print(l2.insert(2, "orange"))
+print(l2.pop(0))
+print(l2.remove("dna"))
+
+
+# task 4
+dict = {"title": "Product", "price": 12, "ingredients": ["meat", "beaf"]}
+print(dict["description"]= "some text bla")
+print(dict.update({"price": 100}))
+
+
